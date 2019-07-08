b424684adc540e0cdb059e4de11405ac078c7e90
Fatima Gomez <fatima3558@gmail.com>
2019-07-08 15:22:29 -0500

    players can personalize names

---
+++ js/Player.js
@@ -0,0 +1,14 @@
+console.log("Player Class");
+
+class Player {
+	constructor() {
+		const name = null
+		const tileColor =  null
+	}
+
+	placeTile() {
+		//loop through spaces in selected column from bottom to top
+		//place tile in the first one that is empty
+	}
+
+}
--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
 console.log("connect 4");
 
+const game = {
+	player1: new Player,
+	player2: new Player,
+
+	startPlayers() {
+		this.player1.name = prompt("Player 1, what's your name?");
+		$(".first h4").text(this.player1.name);
+		// console.log(this.player1.name);
+		this.player2.name = prompt("Player 2, what's your name?");
+		$(".second h4").text(this.player2.name);
+		// console.log(this.player2.name);
+	}
+}
+
+game.startPlayers()
